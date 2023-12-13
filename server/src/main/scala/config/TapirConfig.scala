ee2ec3e58223bd4f33ccd08a346a78d7b98778c7
Andriy Plokhotnyuk <andriy.plokhotnyuk@superfund-technologies.com>
2023-12-13 12:05:56 +0100

    Add commented out options for turning off of auto-pings

--- server/src/main/scala/config/TapirConfig.scala
+++ server/src/main/scala/config/TapirConfig.scala
@@ -16,7 +16,14 @@ import scala.concurrent.duration.*
 object TapirConfig {
   private val wsEndpoint = endpoint.get
     .in("ts")
-    .out(webSocketBody[Long, CodecFormat.TextPlain, Long, CodecFormat.TextPlain](Fs2Streams[IO]))
+    .out(webSocketBody[Long, CodecFormat.TextPlain, Long, CodecFormat.TextPlain](Fs2Streams[IO])
+      /*
+      .concatenateFragmentedFrames(false)
+      .autoPongOnPing(false)
+      .ignorePong(false)
+      .autoPing(None)
+       */
+    )
   private val responseStream = Stream.fixedRate[IO](100.millis, dampen = false).evalMap(_ => IO.realTime.map(_.toMillis))
   private val serverOptions = Http4sServerOptions
     .customiseInterceptors[IO]
